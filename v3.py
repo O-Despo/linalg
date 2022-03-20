2e9301bb469ed35caec05170d150fb6695518761
= <=>
2022-03-20 00:16:16 -0400

    fized right and left

--- v3.py
+++ v3.py
@@ -1,5 +1,6 @@
 from os import rmdir
 from pyexpat.model import XML_CQUANT_REP
+from turtle import color
 import numpy as np
 import matplotlib.pyplot as plt
 import random
@@ -8,9 +9,9 @@ import math
 """One of the key assumtions to make this code simpler is that the middle points is at 0x"""
 def lines_from_triange(points):
     """creates lines must be presented in order of right-middle-left"""
-    right_line_fnc = lambda x: -x*(points[1][1]/points[0][0])+points[1][1]
-    left_line_fnc = lambda x: -x*(points[1][1]/points[2][0])+points[1][1]
-    return right_line_fnc, left_line_fnc
+    left_line_fnc = lambda x: x*(-points[1][1]/points[0][0])+points[1][1]
+    right_line_fnc = lambda x: x*(-points[1][1]/points[2][0])+points[1][1]
+    return  left_line_fnc, right_line_fnc
 
 def flip_over_midpoint(midpoint, point):
     point = [midpoint[0] - (point[0] - midpoint[0]), midpoint[1] - (point[1] - midpoint[1])]
@@ -18,26 +19,27 @@ def flip_over_midpoint(midpoint, point):
 
 def calulate_midpoints(points):
     """creates midpoints must be presented in order of right-middle-left"""
-    right_midpoint = [points[0][0]/2, points[1][1]/2]
-    left_midpoint = [points[2][0]/2, points[1][1]/2]
-    return right_midpoint, left_midpoint
+    left_midpoint = [points[0][0]/2, points[1][1]/2]
+    right_midpoint = [points[2][0]/2, points[1][1]/2]
+    return left_midpoint, right_midpoint
 
-def run_point(point, triangle_points, fncs, mids):
+def run_point(triangle_points, fncs, mids):
     """fncs and mids are diliverd (left, right)"""
-    point[0] = (random.random()*(triangle_points[2][0]-triangle_points[0][0]))-triangle_points[0][0]
+    point = np.zeros((2,))
+    point[0] = (random.random()*(triangle_points[2][0]-triangle_points[0][0]))-triangle_points[2][0]
     point[1] = random.random() * triangle_points[1][1]
 
-    if point[0] < 0:
-        current_fnc = fncs[0]
+    if point[0] < point[1][0]:
+        fnc = fncs[0]
         mid = mids[0] 
     else: 
-        current_fnc = fncs[1] 
+        fnc = fncs[1]
         mid = mids[1] 
         
-    if point[1] > current_fnc(point[0]):
+    if point[1] > fnc(point[0]):
         point = flip_over_midpoint(mid, point)
 
-    return point, current_fnc(point[0])
+    return point, fnc(point[0])
 
 def run_points(x_tri_points, y_tri_points, num_of_points):
     points = np.zeros((num_of_points, 2))
@@ -79,26 +81,30 @@ def run_tri_points(num_of_points, tri_points):
 
     return out_points
         
-
 q = math.sqrt(3)/2
 points_x = np.array([[-0.5,0],[0,math.sqrt(3)/2],[0.5,0]])
 points_y = np.array([[-q*(2/3),0],[0,q],[q*(1/3),0]])
 
 num_of_points = 10000
 
-points = run_points(points_x, points_y, num_of_points)
-# points = run(1000, points_y)
+#Test lines from triangle
+line_fncs = lines_from_triange(points_y)
+
 fig = plt.figure()
-plt.scatter([points[:, 0]], [points[:, 1]], color='red',s =2)
 
 #Graphing
 lfnc, rfnc = lines_from_triange(points_y)
 lmid, rmid = calulate_midpoints(points_y)
 
-plt.plot([-1,0],[lfnc(-1),lfnc(0)])
-plt.plot([1,0],[rfnc(1),rfnc(0)])
+plt.plot([-1,0],[lfnc(-1),lfnc(0)], color="red")
+plt.plot([1,0],[rfnc(1),rfnc(0)], color="blue")
 
 plt.axis([points_x[0][0],points_x[2][0],0,1])
+
+p = [0.2, 0.6]
+z = flip_over_midpoint(rmid ,p)
+plt.scatter([p[0]], [p[1]], color='red')
+plt.scatter([z[0]], [z[1]], color='blue')
 
 plt.scatter([lmid[0]], [lmid[1]], color='red')
 plt.scatter([rmid[0]], [rmid[1]], color='blue')
