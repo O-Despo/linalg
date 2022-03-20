abc41b3c883f4f3a0ea32238c2439e6c9a5738b2
= <=>
2022-03-19 23:52:28 -0400

    failing

--- v3.py
+++ v3.py
@@ -1,14 +1,16 @@
 from os import rmdir
+from pyexpat.model import XML_CQUANT_REP
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+import math
 
 """One of the key assumtions to make this code simpler is that the middle points is at 0x"""
 def lines_from_triange(points):
     """creates lines must be presented in order of right-middle-left"""
     right_line_fnc = lambda x: -x*(points[1][1]/points[0][0])+points[1][1]
     left_line_fnc = lambda x: -x*(points[1][1]/points[2][0])+points[1][1]
-    return left_line_fnc, right_line_fnc
+    return right_line_fnc, left_line_fnc
 
 def flip_over_midpoint(midpoint, point):
     point = [midpoint[0] - (point[0] - midpoint[0]), midpoint[1] - (point[1] - midpoint[1])]
@@ -20,56 +22,87 @@ def calulate_midpoints(points):
     left_midpoint = [points[2][0]/2, points[1][1]/2]
     return right_midpoint, left_midpoint
 
-def run_points(triangle_points, num_new_points):
-    lfnc, rfnc = lines_from_triange(triangle_points)
-    lmid, rmid = calulate_midpoints(triangle_points)
+def run_point(point, triangle_points, fncs, mids):
+    """fncs and mids are diliverd (left, right)"""
+    point[0] = (random.random()*(triangle_points[2][0]-triangle_points[0][0]))-triangle_points[0][0]
+    point[1] = random.random() * triangle_points[1][1]
 
-    new_points = np.zeros((num_new_points, 2))
-    for i in range(num_new_points):
-        x = (random.random()*(triangle_points[2][0]-triangle_points[0][0]))+triangle_points[0][0]
-        y = random.random() * triangle_points[1][1]
-        new_point = [x, y]
-
-        if new_point[0] < 0:
-            current_fnc = rfnc
-            mid = lmid
-        else: 
-            current_fnc = lfnc
-            mid = rmid
+    if point[0] < 0:
+        current_fnc = fncs[0]
+        mid = mids[0] 
+    else: 
+        current_fnc = fncs[1] 
+        mid = mids[1] 
         
-        if new_point[1] > current_fnc(new_point[0]):
-            new_point = flip_over_midpoint(mid, new_point)
+    if point[1] > current_fnc(point[0]):
+        point = flip_over_midpoint(mid, point)
 
-        new_points[i] = new_point
+    return point, current_fnc(point[0])
 
-    return new_points
+def run_points(x_tri_points, y_tri_points, num_of_points):
+    points = np.zeros((num_of_points, 2))
+    y_tri_points_tmp = y_tri_points.copy()
 
-points_x = [[-0.5,0],[0,1],[0.5,0]]
-points_y = [[-2/3,0],[0,1],[1/3,0]]
+    x_point = np.zeros(2,)
+    y_point = np.zeros(2,)
 
-num_of_points = 1000
-new_points_x = run_points(points_x, num_of_points)
-new_points_y = run_points(points_y, num_of_points)
+    eq_fncs = lines_from_triange(x_tri_points)
+    eq_mids = calulate_midpoints(x_tri_points)
+    
+    y_tri_dist =  y_tri_points[0][0] - y_tri_points[2][0]
+    # y_tri_dist = 1
 
-new_points = np.zeros((num_of_points, 2))
-# new_points[:, 0] = new_points_y[:, 0]
-# new_points[:, 1] = new_points_y[:, 0]
+    for i in range(num_of_points):
+        x_point, scaler = run_point(x_point, x_tri_points, eq_fncs, eq_mids)
+        y_tri_points_tmp[0][0] = (y_tri_dist * scaler) + y_tri_points[2][0]
+        y_tri_points_tmp[1][1] = y_tri_points[1][1] - y_tri_points[2][0] * scaler
+        y_fncs = lines_from_triange(y_tri_points_tmp)
+        y_mids = calulate_midpoints(y_tri_points_tmp)
+        y_point, scaler = run_point(y_point, y_tri_points_tmp, y_fncs, y_mids)
+
+        points[i][0] = x_point[0]
+        points[i][1] = y_point[0]
+    
+    return points
+
+def run_tri_points(num_of_points, tri_points):
+    out_points = np.zeros((num_of_points, 2))
+
+    y_fncs = lines_from_triange(tri_points)
+    y_mids = calulate_midpoints(tri_points)
+
+    p = np.zeros(2,)
+    for i in range(num_of_points):
+        run_point(p, tri_points, y_fncs, y_mids)
+        out_points[i][0] = p[0]
+        out_points[i][1] = p[1]
+
+    return out_points
+        
+
+q = math.sqrt(3)/2
+points_x = np.array([[-0.5,0],[0,math.sqrt(3)/2],[0.5,0]])
+points_y = np.array([[-q*(2/3),0],[0,q],[q*(1/3),0]])
+
+num_of_points = 10000
+
+points = run_points(points_x, points_y, num_of_points)
+# points = run(1000, points_y)
 fig = plt.figure()
-plt.scatter(new_points_x[:, 0], new_points_y[:, 0], color='blue')
-
+plt.scatter([points[:, 0]], [points[:, 1]], color='red',s =2)
 
 #Graphing
-lfnc, rfnc = lines_from_triange(points_x)
-lmid, rmid = calulate_midpoints(points_x)
+lfnc, rfnc = lines_from_triange(points_y)
+lmid, rmid = calulate_midpoints(points_y)
 
-
-plt.plot([1,0],[lfnc(1),lfnc(0)])
-plt.plot([-1,0],[rfnc(-1),rfnc(0)])
+plt.plot([-1,0],[lfnc(-1),lfnc(0)])
+plt.plot([1,0],[rfnc(1),rfnc(0)])
 
 plt.axis([points_x[0][0],points_x[2][0],0,1])
 
 plt.scatter([lmid[0]], [lmid[1]], color='red')
 plt.scatter([rmid[0]], [rmid[1]], color='blue')
+plt.grid(True)
 plt.xlabel('x')
 plt.ylabel('y')
 plt.show()
