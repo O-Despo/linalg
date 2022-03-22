204ca12bcb92a6f51556ef406d205a0e1c35bf8d
= <=>
2022-03-21 22:05:32 -0400

    v3 bad base idea

---
+++ test/tri.py
@@ -0,0 +1,36 @@
+from cmath import sqrt
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import math
+
+# make a triangle that is 1/3 the sixe of last choose x bassed on distrobution
+# chosed y with distobution contatinat 
+# 1/3 flip it
+
+h = sqrt(3)/2
+tri_points = np.array([
+    [-0.5, 0.0],
+    [0.0, h],
+    [0.5, 0.0]
+])
+
+def get_funcs(points):
+    # Input left-middle-right
+    lfnc = lambda x : x*((points[1][1]-points[0][1])/(points[1][0]-points[0][0]))
+    rfnc = lambda x : x*((points[1][1]-points[2][1])/(points[1][0]-points[2][0]))
+    return lfnc, rfnc
+
+def get_midpoint(points):
+    # Input two points
+    return [points[0][0]/points[1][0], points[0][1]/points[1][1]]
+
+#Graphing
+lfnc, rfnc = get_funcs(tri_points)
+plt.plot((tri_points[0][0], tri_points[1][0]),(lfnc(tri_points[0][0]), lfnc(tri_points[1][0])))
+plt.plot((tri_points[2][0], tri_points[1][0]),(rfnc(tri_points[2][0]), rfnc(tri_points[1][0])))
+
+lmid = get_midpoint((tri_points[0],tri_points[1]))
+rmid = get_midpoint((tri_points[2],tri_points[1]))
+plt.scatter([lmid[0],rmid[0]], [lmid[1],rmid[1]])
+plt.show()
--- v3.py
+++ v3.py
@@ -1,6 +1,7 @@
 from os import rmdir
 from pyexpat.model import XML_CQUANT_REP
 from turtle import color
+from xml.sax.handler import property_interning_dict
 import numpy as np
 import matplotlib.pyplot as plt
 import random
@@ -9,8 +10,8 @@ import math
 """One of the key assumtions to make this code simpler is that the middle points is at 0x"""
 def lines_from_triange(points):
     """creates lines must be presented in order of right-middle-left"""
-    left_line_fnc = lambda x: x*(-points[1][1]/points[0][0])+points[1][1]
-    right_line_fnc = lambda x: x*(-points[1][1]/points[2][0])+points[1][1]
+    left_line_fnc = lambda x: x*(points[1][1]/-points[0][0])+points[1][1]
+    right_line_fnc = lambda x: x*(points[1][1]/-points[2][0])+points[1][1]
     return  left_line_fnc, right_line_fnc
 
 def flip_over_midpoint(midpoint, point):
@@ -22,6 +23,10 @@ def calulate_midpoints(points):
     left_midpoint = [points[0][0]/2, points[1][1]/2]
     right_midpoint = [points[2][0]/2, points[1][1]/2]
     return left_midpoint, right_midpoint
+
+def caclulate_midpoint_of_2p(points):
+    midpoint = [points[0]/2, points[1]/2]
+    return midpoint
 
 def run_point(triangle_points, fncs, mids, debug=False):
     """fncs and mids are diliverd (left, right)"""
@@ -43,73 +48,77 @@ def run_point(triangle_points, fncs, mids, debug=False):
         point_out = point
 
     if debug:
-        return point_out, point, fnc(point[0])
+        return point_out, point, fnc(point_out[0])
     else:
-        return point, fnc(point[0])
+        return point_out, fnc(point_out[0])
 
 def run_points(x_tri_points, y_tri_points, num_of_points):
     points = np.zeros((num_of_points, 2))
     y_tri_points_tmp = y_tri_points.copy()
 
-    x_point = np.zeros(2,)
-    y_point = np.zeros(2,)
-
-    eq_fncs = lines_from_triange(x_tri_points)
-    eq_mids = calulate_midpoints(x_tri_points)
+    x_fncs = lines_from_triange(x_tri_points)
+    x_mids = calulate_midpoints(x_tri_points)
     
-    y_tri_dist =  y_tri_points[0][0] - y_tri_points[2][0]
-    # y_tri_dist = 1
-
     for i in range(num_of_points):
-        x_point, scaler = run_point(x_point, x_tri_points, eq_fncs, eq_mids)
-        y_tri_points_tmp[0][0] = (y_tri_dist * scaler) + y_tri_points[2][0]
-        y_tri_points_tmp[1][1] = y_tri_points[1][1] - y_tri_points[2][0] * scaler
+        x_point, scaler = run_point(x_tri_points, x_fncs, x_mids)
+
+        y_tri_points_tmp = y_tri_points * scaler
+        
         y_fncs = lines_from_triange(y_tri_points_tmp)
         y_mids = calulate_midpoints(y_tri_points_tmp)
-        y_point, scaler = run_point(y_point, y_tri_points_tmp, y_fncs, y_mids)
+
+        y_point, scalr = run_point(y_tri_points_tmp, y_fncs, y_mids)
 
         points[i][0] = x_point[0]
-        points[i][1] = y_point[0]
+        points[i][1] = -(y_point[0] - y_tri_points_tmp[2][0])
+        # points[i][1] = random.triangular(0,q,q*(1/3)) * scaler
     
     return points
 
 def run_tri_points(num_of_points, tri_points):
     out_points = np.zeros((num_of_points, 2))
 
-    y_fncs = lines_from_triange(tri_points)
-    y_mids = calulate_midpoints(tri_points)
+    fncs = lines_from_triange(tri_points)
+    mids = calulate_midpoints(tri_points)
 
-    p = np.zeros(2,)
     for i in range(num_of_points):
-        run_point(p, tri_points, y_fncs, y_mids)
-        out_points[i][0] = p[0]
-        out_points[i][1] = p[1]
+        p, q = run_point(tri_points, fncs, mids)
+        out_points[i] = p
 
     return out_points
         
 q = math.sqrt(3)/2
-points_x = np.array([[-0.5,0],[0,math.sqrt(3)/2],[0.5,0]])
+points_x = np.array([[-0.5,0],[0,q],[0.5,0]])
 points_y = np.array([[-q*(2/3),0],[0,q],[q*(1/3),0]])
 
-num_of_points = 10000
+num_of_points = 60000
 
 #Test lines from triangle
-line_fncs = lines_from_triange(points_y)
-
+line_fncs = lines_from_triange(points_x)
 fig = plt.figure()
 
 #Graphing
-lfnc, rfnc = lines_from_triange(points_y)
-lmid, rmid = calulate_midpoints(points_y)
+points_in_use = points_x
+lfnc, rfnc = lines_from_triange(points_in_use)
+lmid, rmid = calulate_midpoints(points_in_use)
 
 plt.plot([-1,0],[lfnc(-1),lfnc(0)], color="red")
 plt.plot([1,0],[rfnc(1),rfnc(0)], color="blue")
 
-plt.axis([points_x[0][0],points_x[2][0],0,1])
+plt.axis([points_in_use[0][0],points_in_use[2][0],0,1])
 
-p, z, q = run_point(points_y, (lfnc, rfnc), (lmid, rmid), True)
-plt.scatter([p[0]], [p[1]], color='red')
-plt.scatter([z[0]], [z[1]], color='blue')
+#Debug run point
+# p, z, q = run_point(points_y, (lfnc, rfnc), (lmid, rmid), True)
+# plt.scatter([p[0]], [p[1]], color='red')
+# plt.scatter([z[0]], [z[1]], color='blue')
+
+#Debug run tri
+# pointss = run_tri_points(num_of_points, points_in_use)
+# plt.scatter([pointss[:, 0]], [pointss[:, 1]], color='blue', alpha=0.2)
+
+#Debug run all
+points_graph = run_points(points_x, points_y, num_of_points)
+plt.scatter([points_graph[:, 0]], [points_graph[:, 1]], color='blue', alpha=0.05)
 
 plt.scatter([lmid[0]], [lmid[1]], color='red')
 plt.scatter([rmid[0]], [rmid[1]], color='blue')
