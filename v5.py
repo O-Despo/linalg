b8bf2fb6a4e8aa2b52dc09b896b9bd6022731746
= <=>
2022-03-25 21:32:17 -0400

    fixing v5

---
+++ v5.py
@@ -0,0 +1,123 @@
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection, Line3DCollection
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import math
+
+"""One of the key assumtions to make this code simpler is that the middle points is at 0x"""
+
+class randomPointInTetrahedron():
+    """Produces random points in a tetrahedron without throwing our points
+    Only uses equilateral base"""
+    def __init__(self, points):
+        self.points = points
+        self.sub_points = points
+
+def calc_fncs(points, components=False):
+    """creates lines must be presented in order of right-middle-left"""
+    l_slope = (points[1][1]-points[0][1])/(points[1][0]-points[0][0])
+    l_intercept = points[1][1]
+    r_slope = ((points[1][1]-points[2][1])/(points[1][0]-points[2][0]))
+    r_intercept = points[1][1]
+    if components:
+        return (l_slope, l_intercept), (r_slope, r_intercept)
+    else:
+        return  lambda x: x*l_slope+l_intercept, lambda x: x*r_slope+r_intercept
+
+def flip_over_midpoint(midpoint, point):
+    point = [midpoint[0] - (point[0] - midpoint[0]), midpoint[1] - (point[1] - midpoint[1])]
+    return point
+
+def calulate_midpoints(points):
+    """creates midpoints must be presented in order of right-middle-left"""
+    left_midpoint = [points[0][0]/2, points[1][1]/2]
+    right_midpoint = [points[2][0]/2, points[1][1]/2]
+    return left_midpoint, right_midpoint
+
+def run_line_point():
+    p = [random.random(),random.random()]
+
+    if p[1] < p[0]:
+        p = flip_over_midpoint((0.5, 0.5), p)
+
+    return p
+
+def run_point(triangle_points, fncs, mids):
+    """fncs and mids are diliverd (left, right)"""
+    point = np.zeros((2,))
+    point[0] = (random.random()*(triangle_points[2][0]-triangle_points[0][0]))+triangle_points[0][0]
+    point[1] = random.random() * triangle_points[1][1]
+
+    if point[0] < triangle_points[1][0]:
+        fnc = fncs[0]
+        mid = mids[0] 
+    else: 
+        fnc = fncs[1]
+        mid = mids[1] 
+
+    if point[1] > fnc(point[0]):
+        point_out = flip_over_midpoint(mid, point)
+    else:
+        point_out = point
+
+    return point_out, fnc(point_out[0])
+
+def flip_over_line(slope, y_intercept, p1):
+    d = (p1[0] + (p1[1] - y_intercept)*slope)/(1+slope**2)
+    
+    p1 = [round(2*d - p1[0], 8),round(2*d*slope - p1[1] + 2*y_intercept, 8)]
+    return p1
+
+def run_tri_points(num_of_points, tri_points):
+    out_points = np.zeros((num_of_points, 3))
+    l_line, r_line = calc_fncs(tri_points, components=True)
+    fncs = (lambda x: x*l_line[0]+l_line[1], lambda x: x*r_line[0]+r_line[1])
+    mids = calulate_midpoints(tri_points)
+
+    z_fnc = lambda x: x*(tri_points[1][1]/((tri_points[2][0]-tri_points[0][0])/3)) 
+
+    for i in range(num_of_points):
+        p, q = run_point(tri_points, fncs, mids)
+        a = run_line_point()
+        p = [p[0], q*a[1]]
+
+        dist_to_x = p[1]
+        z = round(random.random()*z_fnc(dist_to_x), 8)
+
+        choice = random.random()
+        if choice < 1/3:
+            p = flip_over_line(l_line[0], l_line[1], p)
+        elif choice < 2/3:
+            p = flip_over_line(r_line[0], r_line[1], p)
+
+        out_points[i] = [round(p[0], 8), round(p[1], 8), z]
+
+    return out_points
+        
+q = math.sqrt(3)/2
+points_x = np.array([[-0.5,0],[0,q],[0.5,0]])
+points_x_sub = np.array([[-0.5,0],[0,q*(1/3)],[0.5,0]])
+
+num_of_points = 25000
+
+#Test lines from triangle
+line_fncs = calc_fncs(points_x)
+
+
+#Graphing
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+
+graph_points = run_tri_points(num_of_points, points_x_sub)
+
+v = np.array([(-0.5, 0, 0), (0.5, 0, 0), (0, points_x[2][0]-points_x[0][0], 0), (0, 1/3, math.sqrt(1/3**2))])
+verts = [ [v[0],v[1],v[2]], [v[0],v[1],v[3]],
+ [v[0],v[2],v[3]], [v[1],v[2],v[3]]]
+
+ax.scatter(graph_points[:, 0], graph_points[:, 1], graph_points[:, 2], color="blue", alpha=0.2)
+ax.add_collection3d(Poly3DCollection(verts, facecolors='r', linewidths=1, edgecolors='r', alpha=.0))
+
+plt.grid(True)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
